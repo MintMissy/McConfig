8fd724075bd310fa6ad1b295782a9cba366c4391
Mint Missy <dawidkostka480@gmail.com>
2023-01-17 20:16:39 +0100

    feat: edit example config

--- src/app/editor/constants/example-config.ts
+++ src/app/editor/constants/example-config.ts
@@ -1,21 +1,36 @@
 export const exampleConfig = {
-	deployment: {
-		test: 'A',
-		files: ['fileA', 'FileB'],
-	},
-	x: 'a',
-	id: 'v1',
-	handlers: [
-		{
-			urlRegex: '/.*',
-			script: {
-				scriptPath: 'example-python-app.py',
+	"Magic Wand": {
+		"v": 3120,
+		"type": "BLAZE_ROD",
+		"meta": {
+			"meta-type": "UNSPECIFIC",
+			"enchants": {
+				"SHARPNESS": 3,
+				"LUCK": 1
 			},
-		},
-	],
-	runtime: 'python27',
-	material: 'ROOTED_DIRT',
-	versionNumber: 10,
-	double: 10.1,
-	threadsafe: false,
+			"Unbreakable": true,
+			"lore": [
+				"&7",
+				"&eRight click to summon fireball &a(120s)"
+			],
+			"display-name": "&eMagic Wand"
+		}
+	},
+	"Golden Pickaxe": {
+		"v": 3120,
+		"type": "GOLDEN_PICKAXE",
+		"meta": {
+			"meta-type": "UNSPECIFIC",
+			"enchants": {
+				"DIG_SPEED": 3,
+				"LOOT_BONUS_BLOCKS": 2
+			},
+			"Unbreakable": true,
+			"lore": [
+				"&7",
+				"&7Right click to dig tunnel &a(90s)"
+			],
+			"display-name": "&e&lGolden Pickaxe"
+		}
+	}
 };
