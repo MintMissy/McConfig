34aab0345d154b0e7028a75eb04ffa50e13d2e48
Mint Missy <dawidkostka480@gmail.com>
2023-01-19 00:54:03 +0100

    test: create tests for file service

--- src/app/config/service/file-service-factory.service.spec.ts
+++ src/app/config/service/file-service-factory.service.spec.ts
@@ -1,17 +1,30 @@
 import { TestBed } from '@angular/core/testing';
+import { ConfigType } from '../enums/config-type.enum';
 import { FileServiceFactory } from './file-service-factory.service';
+import { JsonFileService } from './json-file.service';
+import { YamlFileService } from './yaml-file.service';
 
 describe('ConfigFileServiceFactoryService', () => {
-	let service: FileServiceFactory;
+	let factory: FileServiceFactory;
 
 	beforeEach(() => {
 		TestBed.configureTestingModule({
-			providers: [FileServiceFactory],
+			providers: [FileServiceFactory, JsonFileService, YamlFileService],
 		});
-		service = TestBed.inject(FileServiceFactory);
+		factory = TestBed.inject(FileServiceFactory);
 	});
 
 	it('should be created', () => {
-		expect(service).toBeTruthy();
+		expect(factory).toBeTruthy();
+	});
+
+	it('should create a JSON file service', () => {
+		const fileService = factory.create(ConfigType.JSON);
+		expect(fileService).toBeInstanceOf(JsonFileService);
+	});
+
+	it('should create a YAML file service', () => {
+		const fileService = factory.create(ConfigType.YAML);
+		expect(fileService).toBeInstanceOf(YamlFileService);
 	});
 });
