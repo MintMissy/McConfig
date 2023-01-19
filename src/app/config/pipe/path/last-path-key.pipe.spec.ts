2ab90b07d8c58ddc67a1ec2bcf10f9afa0b6ad95
Mint Missy <dawidkostka480@gmail.com>
2023-01-19 02:32:36 +0100

    test: add tests for last path key

--- src/app/config/pipe/path/last-path-key.pipe.spec.ts
+++ src/app/config/pipe/path/last-path-key.pipe.spec.ts
@@ -1,8 +1,37 @@
 import { LastPathKeyPipe } from './last-path-key.pipe';
 
 describe('LastPathKeyPipe', () => {
+	let pipe: LastPathKeyPipe;
+
+	beforeEach(() => {
+		pipe = new LastPathKeyPipe();
+	});
+	
 	it('create an instance', () => {
-		const pipe = new LastPathKeyPipe();
 		expect(pipe).toBeTruthy();
+	});
+
+	it('should return default key for 1 level nested path', () => {
+		expect(pipe.transform('first')).toBe('first');
+	});
+
+	it('should return last key for 2 level nested path', () => {
+		expect(pipe.transform('first.second')).toBe('second');
+	});
+
+	it('should return last key for 3 level nested path', () => {
+		expect(pipe.transform('first.second.third')).toBe('third');
+	});
+
+	it('should return default key for path that consists of one number', () => {
+		expect(pipe.transform('3')).toBe('3');
+	});
+
+	it('should return last key for 2 level nested path that ends with number', () => {
+		expect(pipe.transform('first.3')).toBe('3');
+	});
+
+	it('should return last key for path that ends with number', () => {
+		expect(pipe.transform('first.second.3')).toBe('3');
 	});
 });
