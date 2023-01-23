870b23a7d61aab126122666e509965b2a06fecf8
Mint Missy <dawidkostka480@gmail.com>
2023-01-23 01:35:07 +0100

    feat: add navbar

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,13 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterModule } from '@angular/router';
 import { SharedModule } from '../shared/shared.module';
 import { FooterComponent } from './ui/footer/footer.component';
 import { NavbarComponent } from './ui/navbar/navbar.component';
 
 @NgModule({
 	declarations: [NavbarComponent, FooterComponent],
-	imports: [CommonModule, SharedModule, MatToolbarModule],
+	imports: [CommonModule, SharedModule, MatToolbarModule, RouterModule],
 	exports: [NavbarComponent, FooterComponent],
 })
 export class CoreModule {}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
-  theme: {
-    extend: {
-      colors:{
-        primary: require('tailwindcss/colors').indigo,
-        secondary: require('tailwindcss/colors').fuchsia,
-        warn: require('tailwindcss/colors').amber,
-        error: require('tailwindcss/colors').rose,
-        light: require('tailwindcss/colors').cyan,
-        dark: require('tailwindcss/colors').slate
-      }
-    },
-  },
-  plugins: [],
-}
+	content: ["./src/**/*.{html,ts}"],
+	theme: {
+		extend: {
+			colors: {
+				primary: require("tailwindcss/colors").indigo,
+				accent: require("tailwindcss/colors").emerald,
+				warn: require("tailwindcss/colors").amber,
+				error: require("tailwindcss/colors").rose,
+				light: require("tailwindcss/colors").cyan,
+				dark: require("tailwindcss/colors").slate,
+			},
+		},
+	},
+	plugins: [],
+};
