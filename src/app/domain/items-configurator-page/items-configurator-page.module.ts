5fb4e529abf036604141982c24f4acfa06e4d0a8
Mint Missy <dawidkostka480@gmail.com>
2023-01-25 00:55:27 +0100

    feat: add inprogress description to items configurator

--- src/app/domain/items-configurator-page/items-configurator-page.module.ts
+++ src/app/domain/items-configurator-page/items-configurator-page.module.ts
@@ -1,11 +1,12 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
+import { MaterialModule } from 'src/app/lib/material/material.module';
 import { ItemsConfiguratorPageRoutingModule } from './items-configurator-page-routing.module';
 import { ItemsConfiguratorPageComponent } from './items-configurator-page.component';
 
 @NgModule({
 	declarations: [ItemsConfiguratorPageComponent],
-	imports: [CommonModule, ItemsConfiguratorPageRoutingModule],
+	imports: [CommonModule, ItemsConfiguratorPageRoutingModule, MaterialModule],
 })
 export class ItemsConfiguratorPageModule {}
