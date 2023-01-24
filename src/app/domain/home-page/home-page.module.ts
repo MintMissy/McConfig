04e56d4062a15275991d782da8a5db1e8f5ed378
Mint Missy <dawidkostka480@gmail.com>
2023-01-24 18:53:59 +0100

    feat: home page, style updates

--- src/app/domain/home-page/home-page.module.ts
+++ src/app/domain/home-page/home-page.module.ts
@@ -1,17 +1,13 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
+import { SharedModule } from 'src/app/shared/shared.module';
 import { HomePageRoutingModule } from './home-page-routing.module';
+import { HomePageComponent } from './home-page.component';
 import { SubPageCardComponent } from './ui/sub-page-card/sub-page-card.component';
 
-
 @NgModule({
-  declarations: [
-    SubPageCardComponent
-  ],
-  imports: [
-    CommonModule,
-    HomePageRoutingModule
-  ]
+	declarations: [SubPageCardComponent, HomePageComponent],
+	imports: [CommonModule, HomePageRoutingModule, SharedModule],
 })
-export class HomePageModule { }
+export class HomePageModule {}
